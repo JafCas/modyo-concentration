b5ea2e91f5d75350fd2d034730877681932209a4
Jafet Castillo <jafetkaid@gmail.com>
2025-03-31 16:52:39 -0600

    Add flex direction to have a header

--- src/screens/memoryGame/MemoryGame.tsx
+++ src/screens/memoryGame/MemoryGame.tsx
@@ -73,7 +73,7 @@ const MemoryGame = ({ onGameOver }: MemoryGameProps) => {
     <div>
       <div className="game-container">
         <div className="card-container">
-          <div>
+          <div style={{ display: "flex", flexDirection: "row", gap: "40px", alignItems: "center" }}>
             <h2>Memory Game</h2>
             <p>Correct Matches: {correctCount}</p>
             <p>Incorrect Matches: {incorrectCount}</p>
